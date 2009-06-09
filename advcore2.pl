96ba30ee6c38e9580e6c67be3e79feb7cdbb3470
Adrian Prantl <adrian@f4z.org>
2009-06-09 13:37:54 -0700

    newline
    darcs-hash:20090609203754-12e4a-7fd289b5ddd7e0947264f3ddf61d16fb82df2969.gz

--- advcore2.pl
+++ advcore2.pl
@@ -2,10 +2,12 @@
 % (C) 2007-2009 Adrian Prantl
 :- module(advcore2, [main/0]).
 
-main:-
+main :-
   cformat('Welcome!~n', []),
   cwrite('>'),
   io_loop([],[]).
+
+bye :- cformat('~nGoodbye!~n', []).
 
 cwrite(T) :- write_xy(T, -1, -1).
 cformat(Format, Arguments) :-
@@ -16,17 +18,30 @@ format_xy(Format, Arguments, X, Y) :-
   write_xy(T, X, Y).
 
 io_loop(WordsR, CsR) :-
-  format_xy('Words = ~w, Cs = ~w~n', [WordsR,CsR], 0, 22),
+  format_xy('Words = ~w, Cs = ~w~n', [WordsR,CsR], 0, 23),
+  autocomplete(WordsR, CsR),
   getch(Char), !,
-  format_xy('Char = ~c (~w)~n', [Char,Char], 24, 0),
+  format_xy('Char = ~c (~w)~n', [Char,Char], 64, 0),
   handle_char(Char, WordsR, CsR).
 
-% Switch over curren Char
-handle_char(end_of_file, _, _) :- !.
-handle_char(C, WsR, CsR) :- char_code(' ', C),  !, handle_whitespace(WsR, CsR).
-handle_char(C, WsR, CsR) :-      backspace(C),  !, handle_backspace(WsR, CsR).
-handle_char(C, WsR, CsR) :- char_code('\t', C), !, autocomplete(WsR, CsR).
-handle_char(C, WsR, CsR) :- io_loop(WsR, [C|CsR]).
+% Switch over current Char
+handle_char(C, WsR, CsR) :- char_code(' ', C),   handle_whitespace(WsR, CsR).
+handle_char(C, WsR, CsR) :- char_code('\t', C),  handle_tabulator(WsR, CsR).
+handle_char(C, WsR, CsR) :- backspace(C),        handle_backspace(WsR, CsR).
+handle_char(C, WsR, CsR) :- char_type(C, alnum), io_loop(WsR, [C|CsR]).
+handle_char(C,_WsR,_CsR) :- char_type(C, end_of_file), bye.
+handle_char(C, WsR, CsR) :- char_type(C, end_of_line), 
+  reverse(CsR, Cs),
+  atom_codes(W, Cs),
+  reverse(Sentence, [W|WsR]),
+  (phrase(sentence(A), Sentence)
+  -> cwrite('\n'),
+   action(A),
+   io_loop([], [])
+  ; cwrite('Sorry, I could not understand that!'),
+   io_loop(WsR, CsR)
+  ).
+
 
 %-----------------------------------------------
 % Ignore Whitespace
@@ -37,10 +52,13 @@ handle_whitespace(WordsR, CsR) :-
   reverse(CsR, Cs),
   atom_codes(W, Cs),
   io_loop([W|WordsR], []).
+handle_tabulator(WordsR, CsR) :-
+  cwrite('\b\b\b\b'),
+  io_loop([W|WordsR], [CsR]).
 
 %-----------------------------------------------
 % Handle Backspace
-backspace(127) :- cwrite('\b').
+backspace(127) :- cwrite('\b\b\b   \b\b\b').
 backspace(8).
 
 handle_backspace([], []) :- !,
@@ -54,43 +72,37 @@ handle_backspace(WordsR, [_|CsR]) :-
 
 %-----------------------------------------------
 % Autocompletion
-letters(Ls) :-
-  length(Ls, _N),
-  maplist(letter, Ls).
 
-letter(L) :- char_type(L, alpha).
-
-finishes_sentence(WordsR, CsR, Letters, Words) :-
+finishes_sentence(WordsR, CsR, Suffix, Words) :-
   reverse(Cs, CsR),
   atom_codes(W1, Cs),
   % Append Letters
-  letters(Letters),
-  concat_atom([W1|Letters], W2),
+  word(W2),
+  atom_concat(W1, Suffix, W2),
   % ... and words
   reverse(Ws, [W2|WordsR]),
   append(Ws, Words, CsX),
   % and find an autocompletion
   phrase(sentence(_), CsX).
-
+finishes_sentence(_, _, ' ', []) :- repeat.
 
 autocomplete(WordsR, CsR) :-
-  cwrite('\b\b\b\b\b['),
+  %cwrite('\b\b\b\b\b['),
   recorda(count, 0),
-  repeat,
 
-  finishes_sentence(WordsR, CsR, AC_Letters, AC_Words),
-  % Console output
-  concat_atom(AC_Letters, A), cwrite(A),
-  maplist(cwrite_, AC_Words),
-  cwrite('\n'),
+  finishes_sentence(WordsR, CsR, Suffix, AC_Words),
+    % Console output
+    once(recorded(count, N)),
+    %trace, writeln(N),
+    Line is 1 + N,
+    append([[Suffix], AC_Words, ['.','','','']], S),
+    format_xy('~w ~w ~w ~w ~w~n', S, 40, Line),
+    N1 is N+1, recorda(count, N1),
+  (N >= 3 -> true; fail).
 
-  recorded(count, N), N1 is N+1,
-  recorda(count, N1),
-  (N > 3 -> true; fail),
-
-  cwrite(']\n'),
-  print_line(WordsR, CsR),
-  io_loop(WordsR, CsR).
+  %cwrite(']\n'),
+  %print_line(WordsR, CsR),
+  %io_loop(WordsR, CsR).
 autocomplete(_, _) :- cwrite('ERROR').
 
 cwrite_(X) :- cwrite(' '), cwrite(X).
@@ -103,32 +115,65 @@ print_line(WordsR, CsR) :-
   maplist(cwrite_, Words),
   cwrite(W).
 
+
+action(X) :- cformat('~w~n',X).
+
+%-----------------------------------------------
+% BASIC GAME MECHANICS
+
+object(light).
+object(bread).
+object(door).
+object(lighter).
+
+person(gnome).
+
+material(door, wood).
+inflammable(wood).
+bibulous(wood).
+
 %-----------------------------------------------
 % GRAMMAR
+
+word(W) :- phrase(det, Ws), member(W, Ws).
+word(W) :- phrase(noun(_,_), Ws), member(W, Ws).
+word(W) :- phrase(verb(_,_), Ws), member(W, Ws).
+word(W) :- phrase(adverb(_), Ws), member(W, Ws).
+word(W) :- phrase(preposition(_), Ws), member(W, Ws).
+
 sentence([Verb, Noun]) --> verb(Type, Verb), nounphrase(Type, Noun).
 
 det --> [the].
-det --> [a].
-det --> [an].
+det --> [my].
+%det --> [a].
+%det --> [an].
 
 nounphrase(Type,Noun) --> det,noun(Type,Noun).
-nounphrase(Type,Noun) --> noun(Type,Noun).
+%nounphrase(Type,Noun) --> noun(Type,Noun).
 
-noun(thing,light) --> [light].
+noun(object, Object) --> { object(Object) }, [Object].
+noun(person, Person) --> { person(Person) }, [Person].
 
-verb(thing,V) --> tran_verb(V).
-verb(intran,V) --> intran_verb(V).
+verb(person,V) --> trans_verb(person, V).
+verb(object,V) --> trans_verb(object, V).
+verb(intrans,V) --> intrans_verb(V).
 
+preposition(at) --> [at].
+preposition(in) --> [in].
+preposition(to) --> [to].
 
-tran_verb(take) --> [take].
-tran_verb(drop) --> [drop].
-tran_verb(eat) --> [eat].
-tran_verb(turn_on) --> [turn,on].
-tran_verb(turn_off) --> [turn,off].
-tran_verb(talk_to) --> [talk,to].
-tran_verb(look_in) --> [look,in].
-tran_verb(look_at) --> [look,at].
+adverb(on) --> [on].
+adverb(off) --> [off].
 
-intran_verb(inventory) --> [inventory].
-intran_verb(look) --> [look].
-intran_verb(quit) --> [exit].
+trans_verb(object, take) --> [take].
+trans_verb(object, drop) --> [drop].
+trans_verb(object, eat) --> [eat].
+trans_verb(object, turn_on) --> [turn,on].
+trans_verb(object, turn_off) --> [turn,off].
+trans_verb(person, talk_to) --> [talk,to].
+trans_verb(object, look_in) --> [look,in].
+trans_verb(_, look_at) --> [look,at].
+
+intrans_verb(inventory) --> [inventory].
+intrans_verb(look) --> [look].
+intrans_verb(quit) --> [exit].
