fe9a84df3a734f7a504edfff5305126f07d23205
Adrian Prantl <adrian@f4z.org>
2009-11-08 23:21:06 -0800

    UI?
    darcs-hash:20091109072106-12e4a-e061063d711502bc5981cc98ca0371bae24a7f16.gz

---
+++ adventure/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  adventure
+//
+//  Created by Adrian Prantl on 08.11.09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
