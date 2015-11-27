620b5caf4ecd2e2ebddec5adc23afe0c727e4a04
Adrian Prantl <adrian@f4z.org>
2015-11-26 17:25:03 -0800

    change expected hostname

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 basepath=/
-if [ x`hostname` = xf4z.org ]; then
+if [ x`hostname` = xf4z ]; then
     basepath=/adrian/adventure/
 fi
 swipl -O -G64m -L64m -T64m -f advserver.pl "$basepath"
